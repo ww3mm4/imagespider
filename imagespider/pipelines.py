913a84945a83ef81f8174c07f8d076d283aba9ad
zhangyao <1657964707@qq.com>
2018-05-22 16:51:24 +0800

    Update pipelines.py

--- imagespider/pipelines.py
+++ imagespider/pipelines.py
@@ -3,7 +3,7 @@
 # Define your item pipelines here
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+## See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 import scrapy
 import sys
 from scrapy.contrib.pipeline.images import ImagesPipeline, Image
